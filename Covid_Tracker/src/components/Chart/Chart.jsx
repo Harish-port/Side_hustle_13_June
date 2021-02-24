dd9ae0f8cae1107d228e551fe3d0dbf13fe2f5e0
Harish-port <harish.sq@gmail.com>
2021-02-24 23:30:25 +0530

    few changes in UI

---
+++ Covid_Tracker/src/components/Chart/Chart.jsx
@@ -0,0 +1,15 @@
+import React,{useState,useEffect} from 'react';
+import {fetchDailyData} from '../../api';
+export default function Chart() {
+    const [dailyData,setDailyData] =useState({});
+    useEffect(()=>{
+        const fetchAPI = async () => {
+            setDailyData(await fetchDailyData());
+        }
+        fetchAPI();
+    })
+    return (
+        <div>
+        </div>
+    )
+}
