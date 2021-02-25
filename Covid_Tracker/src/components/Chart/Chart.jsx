8e1d0e2d4e0242c42fc1dc8b2b9caa5e6644a46a
Harish-port <harish.sq@gmail.com>
2021-02-26 01:15:49 +0530

    search bar added feature

--- Covid_Tracker/src/components/Chart/Chart.jsx
+++ Covid_Tracker/src/components/Chart/Chart.jsx
@@ -1,15 +1,41 @@
 import React,{useState,useEffect} from 'react';
 import {fetchDailyData} from '../../api';
+import {Line,Bar} from 'react-chartjs-2';
+import styles from './Chart.module.css';
+
 export default function Chart() {
-    const [dailyData,setDailyData] =useState({});
+    const [dailyData,setDailyData] =useState([]);
     useEffect(()=>{
         const fetchAPI = async () => {
             setDailyData(await fetchDailyData());
         }
         fetchAPI();
     })
+    const lineChart = (
+        
+dailyData.length ?
+( 
+<Line 
+data = {{
+    labels:dailyData.map(({date})=>date),
+    datasets:[{
+        data:dailyData.map(({confirmed})=>confirmed),
+        label:"Infected",
+        borderColor:"#3333ff",
+        fill:true,
+    },{
+        data:dailyData.map(({deaths})=>deaths),
+        label:"Deaths",
+        backgroundColor:"rgba(255,0,0,0.5)",
+        borderColor:"red",
+        fill:true,
+    }],
+}}
+/>):null
+    )
     return (
-        <div>
+        <div className={styles.container}>
+            {lineChart}
         </div>
     )
 }
--- Covid_Tracker/src/components/CountryPicker/CountryPicker.jsx
+++ Covid_Tracker/src/components/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { NativeSelect, FormControl } from '@material-ui/core';
+import { fetchCountries } from '../../api';
+import styles from './CountryPicker.module.css'
 
-export default function CountryPicker() {
+const CountryPicker = () => {
+    const [fetchedCountries,setFetchedCountries] = useState([]);
+    useEffect(()=>{
+        const fetchAPI = async () =>{
+            setFetchedCountries(await fetchCountries());
+        }
+        fetchAPI()
+    },[setFetchedCountries])
+    console.log(fetchedCountries);
     return (
-        <div>
-            
-        </div>
+        <FormControl className={styles.formControl}>
+            <NativeSelect>
+                <option value="global">Global</option>
+                {fetchedCountries.map((country,i)=><option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
     )
 }
+export default CountryPicker
