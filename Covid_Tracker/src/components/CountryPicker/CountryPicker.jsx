0420f0a4b2437f890537558e8bfc0a49a640a78c
Harish-port <harish.sq@gmail.com>
2021-02-28 20:09:41 +0530

    changes

--- Covid_Tracker/src/components/CountryPicker/CountryPicker.jsx
+++ Covid_Tracker/src/components/CountryPicker/CountryPicker.jsx
@@ -3,22 +3,24 @@ import { NativeSelect, FormControl } from '@material-ui/core';
 import { fetchCountries } from '../../api';
 import styles from './CountryPicker.module.css'
 
-const CountryPicker = () => {
+const CountryPicker = ({handleCountryChange}) => {
     const [fetchedCountries,setFetchedCountries] = useState([]);
     useEffect(()=>{
         const fetchAPI = async () =>{
             setFetchedCountries(await fetchCountries());
         }
-        fetchAPI()
+        fetchAPI();
     },[setFetchedCountries])
     console.log(fetchedCountries);
+
     return (
         <FormControl className={styles.formControl}>
-            <NativeSelect>
-                <option value="global">Global</option>
+            <NativeSelect defaultValue= "" onChange={(e)=>handleCountryChange(e.target.value)}>
+                <option value="">Global</option>
                 {fetchedCountries.map((country,i)=><option key={i} value={country}>{country}</option>)}
             </NativeSelect>
         </FormControl>
     )
 }
+
 export default CountryPicker
